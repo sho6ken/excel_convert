28a088d9c2c71567dca9149830f8275116268db6
Kent <>
2024-11-25 18:15:03 +0800

    feat: json輸出器

---
+++ Assets/scripts/exporter/json_exporter_c.cs
@@ -0,0 +1,12 @@
+namespace mohist.excel.exporter {
+    /// <summary>
+    /// 輸出json文件
+    /// </summary>
+    /// <remarks>client用</remarks>
+    public class JsonExporterC : JsonExporter {
+        /// <summary>
+        /// 目標用戶型態
+        /// </summary>
+        protected override UserType _user { get { return UserType.Client; } }
+    }
+}
