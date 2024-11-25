39e9f1b734cc80f745a3af6a2a29a28c7e91aa53
Kent <>
2024-11-25 18:14:38 +0800

    feat: excel轉檔核心

---
+++ Assets/scripts/core/define.cs
@@ -0,0 +1,51 @@
+namespace mohist.excel {
+    /// <summary>
+    /// 定義
+    /// </summary>
+    public class Define {
+        /// <summary>
+        /// 欄位名稱列
+        /// </summary>
+        public const int NAME_ROW = 1;
+
+        /// <summary>
+        /// 欄位型態列
+        /// </summary>
+        public const int TYPE_ROW = 2;
+
+        /// <summary>
+        /// 用戶種類列
+        /// </summary>
+        public const int USER_ROW = 3;
+
+        /// <summary>
+        /// 資料開始列
+        /// </summary>
+        public const int DATA_START_ROW = 4;
+    }
+
+    /// <summary>
+    /// 用戶種類
+    /// </summary>
+    public enum UserType {
+        /// <summary>
+        /// 禁用
+        /// </summary>
+        Forbid,
+
+        /// <summary>
+        /// client用
+        /// </summary>
+        Client,
+
+        /// <summary>
+        /// server用
+        /// </summary>
+        Server,
+
+        /// <summary>
+        /// 通用
+        /// </summary>
+        Everyone,
+    }
+}
